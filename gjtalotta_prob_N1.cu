8e0e3fd14f044acf59ee5561fa16a852ba5a6952
Gregory Talotta <gjtalotta@alaska.edu>
2021-04-22 14:45:48 -0900

    zcsx

--- gjtalotta_prob_N1.cu
+++ gjtalotta_prob_N1.cu
@@ -166,7 +166,7 @@ int main()
   // 
   int * c;
   int * dev_c;
-  c = (int *)malloc(sizeof(int) * imgWidth * imgHeight);
+  // c = (int *)malloc(sizeof(int) * imgWidth * imgHeight);
   // cudaMalloc((void**)&dev_c, sizeof(int) * imgWidth * imgHeight);
   printf("preconvert");
   // Convert image into a flat array of chars with the value 0-255 of the
