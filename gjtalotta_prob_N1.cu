ed23e4fc0f20cf6cd1cc01dd18816c31bb6ec353
Gregory Talotta <gjtalotta@alaska.edu>
2021-04-22 16:06:25 -0900

    might just work

--- gjtalotta_prob_N1.cu
+++ gjtalotta_prob_N1.cu
@@ -20,85 +20,109 @@
 // Given te desired x,y, and image width
 int pixelIndex(int x, int y, int width)
 {
-    return (y*width + x);
+  return (y * width + x);
 }
 
 // Returns the sobel value for pixel x,y
-int sobel(int x, int y, int width, char *pixels)
+__global__ void sobel(int width, char *pixels, int *c)
 {
-   int x00 = -1;  int x20 = 1;
-   int x01 = -2;  int x21 = 2;
-   int x02 = -1;  int x22 = 1;
-   x00 *= pixels[pixelIndex(x-1,y-1,width)];
-   x01 *= pixels[pixelIndex(x-1,y,width)];
-   x02 *= pixels[pixelIndex(x-1,y+1,width)];
-   x20 *= pixels[pixelIndex(x+1,y-1,width)];
-   x21 *= pixels[pixelIndex(x+1,y,width)];
-   x22 *= pixels[pixelIndex(x+1,y+1,width)];
-   
-   int y00 = -1;  int y10 = -2;  int y20 = -1;
-   int y02 = 1;  int y12 = 2;  int y22 = 1;
-   y00 *= pixels[pixelIndex(x-1,y-1,width)];
-   y10 *= pixels[pixelIndex(x,y-1,width)];
-   y20 *= pixels[pixelIndex(x+1,y-1,width)];
-   y02 *= pixels[pixelIndex(x-1,y+1,width)];
-   y12 *= pixels[pixelIndex(x,y+1,width)];
-   y22 *= pixels[pixelIndex(x+1,y+1,width)];
+  int x00 = -1;
+  int x20 = 1;
+  int x01 = -2;
+  int x21 = 2;
+  int x02 = -1;
+  int x22 = 1;
+  x00 *= pixels[pixelIndex(x - 1, y - 1, width)];
+  x01 *= pixels[pixelIndex(x - 1, y, width)];
+  x02 *= pixels[pixelIndex(x - 1, y + 1, width)];
+  x20 *= pixels[pixelIndex(x + 1, y - 1, width)];
+  x21 *= pixels[pixelIndex(x + 1, y, width)];
+  x22 *= pixels[pixelIndex(x + 1, y + 1, width)];
 
-   int px = x00 + x01 + x02 + x20 + x21 + x22;
-   int py = y00 + y10 + y20 + y02 + y12 + y22;
-   return sqrt(px*px + py*py);
+  int y00 = -1;
+  int y10 = -2;
+  int y20 = -1;
+  int y02 = 1;
+  int y12 = 2;
+  int y22 = 1;
+  y00 *= pixels[pixelIndex(x - 1, y - 1, width)];
+  y10 *= pixels[pixelIndex(x, y - 1, width)];
+  y20 *= pixels[pixelIndex(x + 1, y - 1, width)];
+  y02 *= pixels[pixelIndex(x - 1, y + 1, width)];
+  y12 *= pixels[pixelIndex(x, y + 1, width)];
+  y22 *= pixels[pixelIndex(x + 1, y + 1, width)];
+
+  int px = x00 + x01 + x02 + x20 + x21 + x22;
+  int py = y00 + y10 + y20 + y02 + y12 + y22;
+  c[pixelIndex(blockIdx.x, blockIdx.y, width)] = sqrt(px * px + py * py);
 }
 
 int main()
 {
-    FreeImage_Initialise();
-    atexit(FreeImage_DeInitialise);
+  FreeImage_Initialise();
+  atexit(FreeImage_DeInitialise);
 
-    // Load image and get the width and height
-    FIBITMAP *image;
-    image = FreeImage_Load(FIF_PNG, "coins.png", 0);
-    if (image == NULL)
+  // Load image and get the width and height
+  FIBITMAP *image;
+  image = FreeImage_Load(FIF_PNG, "coins.png", 0);
+  if (image == NULL)
+  {
+    printf("Image Load Problem\n");
+    exit(0);
+  }
+  int imgWidth;
+  int imgHeight;
+  imgWidth = FreeImage_GetWidth(image);
+  imgHeight = FreeImage_GetHeight(image);
+
+  int *c;
+  int *dev_c;
+  c = (int *)malloc(sizeof(int) * imgWidth * imgHeight);
+  cudaMalloc((void **)&dev_c, sizeof(int) * imgWidth * imgHeight);
+
+  // Convert image into a flat array of chars with the value 0-255 of the
+  // greyscale intensity
+  RGBQUAD aPixel;
+  char *pixels;
+  int pixIndex = 0;
+  pixels = (char *)malloc(sizeof(char) * imgWidth * imgHeight);
+  for (int i = 0; i < imgHeight; i++)
+    for (int j = 0; j < imgWidth; j++)
     {
-        printf("Image Load Problem\n");
-        exit(0);
+      FreeImage_GetPixelColor(image, j, i, &aPixel);
+      char grey = ((aPixel.rgbRed + aPixel.rgbGreen + aPixel.rgbBlue) / 3);
+      pixels[pixIndex++] = grey;
     }
-    int imgWidth;
-    int imgHeight;
-    imgWidth = FreeImage_GetWidth(image);
-    imgHeight = FreeImage_GetHeight(image);
 
-    // Convert image into a flat array of chars with the value 0-255 of the
-    // greyscale intensity
-    RGBQUAD aPixel;
-    char *pixels;  
-    int pixIndex = 0;
-    pixels = (char *) malloc(sizeof(char)*imgWidth*imgHeight);
-    for (int i = 0; i < imgHeight; i++)
-     for (int j = 0; j < imgWidth; j++)
-     {
-       FreeImage_GetPixelColor(image,j,i,&aPixel);
-       char grey = ((aPixel.rgbRed + aPixel.rgbGreen + aPixel.rgbBlue)/3);
-       pixels[pixIndex++]=grey;
-     }
+  char *dev_pixels;
+  cudaMalloc((void **)&dev_pixels, sizeof(char) * imgWidth * imgHeight);
+  cudaMemcpy(dev_pixels, pixels, sizeof(char) * imgWidth * imgHeight, cudaMemcpyHostToDevice);
 
-    // Apply sobel operator to pixels, ignoring the borders
-    FIBITMAP *bitmap = FreeImage_Allocate(imgWidth, imgHeight, 24);
-    for (int i = 1; i < imgWidth-1; i++)
+
+  // ** change
+  dim3 threadsPerBlock(1);             //one thred per block
+  dim3 numBlocks(imgWidth, imgHeight); // one block per pixel
+  sobel<<<numBlocks, threadsPerBlock>>>(imgWidth, dev_pixels, dev_c);
+  cudaMemcpy(c, dev_c, sizeof(int) * imgWidth * imgHeight, cudaMemcpyDeviceToHost);
+
+
+  // Apply sobel operator to pixels, ignoring the borders ** change to use c arr
+  FIBITMAP *bitmap = FreeImage_Allocate(imgWidth, imgHeight, 24);
+  for (int i = 1; i < imgWidth - 1; i++)
+  {
+    for (int j = 1; j < imgHeight - 1; j++)
     {
-      for (int j = 1; j < imgHeight-1; j++)
-      {
-	int sVal = sobel(i,j,imgWidth,pixels);
-	aPixel.rgbRed = sVal;
-	aPixel.rgbGreen = sVal;
-	aPixel.rgbBlue = sVal;
-        FreeImage_SetPixelColor(bitmap, i, j, &aPixel);
-      }
+      int sVal = sobel(i, j, imgWidth, pixels); //change to use arr answers
+      aPixel.rgbRed = sVal;
+      aPixel.rgbGreen = sVal;
+      aPixel.rgbBlue = sVal;
+      FreeImage_SetPixelColor(bitmap, i, j, &aPixel);
     }
-    FreeImage_Save(FIF_PNG, bitmap, "coins-edge.png", 0);
-  
-    free(pixels);
-    FreeImage_Unload(bitmap);
-    FreeImage_Unload(image);
-    return 0;
+  }
+  FreeImage_Save(FIF_PNG, bitmap, "coins-edge.png", 0);
+
+  free(pixels);
+  FreeImage_Unload(bitmap);
+  FreeImage_Unload(image);
+  return 0;
 }
